5bf8bea0ec31e6be6c65ed7c0ce35c59adc57178
andrzej-t <andrzej.tyrpa@gmail.com>
2021-04-16 22:11:53 +0200

    Exercise 14.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,4 +1,34 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Calculator {
+
+    @Autowired
+    Display display;
+
+    double a = 2.0;
+    double b = 3.0;
+
+    public double add(double a, double b) {
+        display.displayValue(a+b);
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        display.displayValue(a-b);
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        display.displayValue(a*b);
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        display.displayValue(a/b);
+        return a / b;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,24 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CalculatorTestSuite {
+
+    @Autowired
+    Calculator calculator;
+
+    @Test
+    void testCalculations() {
+        //Given
+        //When
+        calculator.display.displayValue(calculator.a + calculator.b);
+        calculator.display.displayValue(calculator.a - calculator.b);
+        calculator.display.displayValue(calculator.a * calculator.b);
+        calculator.display.displayValue(calculator.a / calculator.b);
+        //Then
+        //do nothing
+    }
 }
