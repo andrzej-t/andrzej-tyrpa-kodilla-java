1e78153633aceb247d43f91182627381e710dbea
andrzej-t <andrzej.tyrpa@gmail.com>
2021-08-19 18:59:02 +0200

    Exercise 31.1. Ebay on Selenium

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,13 +1,25 @@
 package com.kodilla.testing2.google;
 
 import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
+
+    public static final String SEARCHFIELD = "q";
+    public static final String ALLOWCOOKIES = "L2AGLb";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.google.com");
+
+        WebElement searchField = driver.findElement(By.id(ALLOWCOOKIES));
+        searchField.click();
+
+        searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
 
     }
 
